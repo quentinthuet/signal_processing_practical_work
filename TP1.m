72e12e653f879d808a7c212ceb80e186348d9367
quentinthuet <quentin.thuet@gmail.com>
2020-01-06 13:15:31 +0100

    Màj

--- TP1.m
+++ TP1.m
@@ -13,7 +13,7 @@ x = A*cos(2*pi*f0*t);
 %Figure 1
 figure(1);
 plot(t,x);
-title('Cos de frÃ©q 1100Hz et d''ampl 1 V Ã©chantil 90 fois Ã  la frÃ©q 10000Hz')
+title('Cos de fréq 1100Hz et d''ampl 1 V échantil 90 fois à la fréq 10000Hz')
 xlabel('temps (s)')
 ylabel('amplitude (V)')
 
@@ -27,7 +27,7 @@ x = A*cos(2*pi*f0*t);
 %Figure 2
 figure(2);
 plot(t,x);
-title('Cos de frÃ©q 1100Hz et d''ampl 1 V Ã©chant 90 fois Ã  la frÃ©q 1000Hz')
+title('Cos de fréq 1100Hz et d''ampl 1 V échant 90 fois à la fréq 1000Hz')
 xlabel('temps (s)')
 ylabel('amplitude (V)')
 
@@ -51,7 +51,7 @@ f = linspace(-Fe/2,Fe/2,N);
 figure(3);
 semilogy(f,Xabs);
 title('TFD du cos de la Figure 1')
-xlabel('frÃ©quence (Hz)')
+xlabel('fréquence (Hz)')
 ylabel('amplitude (V)')
 
 %(b)
@@ -70,11 +70,144 @@ f = linspace(-Fe/2,Fe/2,N);
 figure(4);
 semilogy(f,Xabs);
 title('TFD du cos de la Figure 2')
-xlabel('frÃ©quence (Hz)')
+xlabel('fréquence (Hz)')
 ylabel('amplitude (V)')
 
+%4.
 
+Fe = 10000;
 
+t = linspace(0,(N-1)/Fe,N);
+x = A*cos(2*pi*f0*t);
+
+M  = 10;
+Np = 2^nextpow2(M*N);
+
+y = zeros(1,Np);
+y(1,1:length(x)) = x;
+Y = fftshift(fft(y,Np));
+
+Yabs = abs(Y);
+
+f = linspace(-Fe/2,Fe/2,Np);
+
+%Figure 5
+figure(5);
+plot(f,Yabs);
+title('TFD du cos de la figure 1 après Zero Padding')
+xlabel('fréquence (Hz)')
+ylabel('amplitude (V)')
+lg = legend('$N_{p} = 1024$', ...
+	'Location','Best');
+set(lg,'Interpreter','Latex');
+
+%5.
+
+M1 = 1;
+M2 = 2;
+M3 = 10;
+
+Np1 = 2^nextpow2(M1*N);
+Np2 = 2^nextpow2(M2*N);
+Np3 = 2^nextpow2(M3*N);
+
+y1 = zeros(1,M1*N);
+y2 = zeros(1,M2*N);
+y3 = zeros(1,M3*N);
+
+y1(1,1:length(x)) = x;
+y2(1,1:length(x)) = x;
+y3(1,1:length(x)) = x;
+
+Y1 = fftshift(fft(y,Np1));
+Y2 = fftshift(fft(y,Np2));
+Y3 = fftshift(fft(y,Np3));
+
+Yabs1 = abs(Y1);
+Yabs2 = abs(Y2);
+Yabs3 = abs(Y3);
+
+f1 = linspace(-Fe/2,Fe/2,Np1);
+f2 = linspace(-Fe/2,Fe/2,Np2);
+f3 = linspace(-Fe/2,Fe/2,Np3);
+
+%Figure 6
+figure(6);
+semilogy(f1,Yabs1,f2,Yabs2,f3,Yabs3);
+title('TFD du cos de la figure 1 après Zero Padding')
+xlabel('fréquence (Hz)')
+ylabel('amplitude (V)')
+lg = legend('$N_{p} = 128$', ...
+	'$N_{p} = 256$', ...
+	'$N_{p} = 1024$', ...
+	'Location','Best');
+set(lg,'Interpreter','Latex');
+
+t = linspace(0,(N-1)/Fe,N);
+x = ifft(Y3);
+
+%%%%5.4%%%
+
+%1.
+
+k = linspace(0,(N-1),N);
+x = A*cos(2*pi*f0*k/Fe+rand*2*pi);
+
+%2.
+
+S_x = 1/N*abs(fft(x)).^2;
+f = linspace(0,Fe,N);
+
+%Figure 7
+figure(7);
+plot(f,S_x);
+title('Estimation par périodogramme de la DSP du cos numérique aléatoire généré')
+xlabel('fréquence (Hz)')
+ylabel('amplitude (V)')
+
+%3.
+
+S_x_hamming = 1/N*abs(fft(x.*hamming(length(x))')).^2;
+S_x_blackman = 1/N*abs(fft(x.*blackman(length(x))')).^2;
+
+%Figure 8
+figure(8);
+plot(f,S_x_hamming,f,S_x_blackman);
+title('Estimation par périodogramme fenêtré')
+xlabel('fréquence (Hz)')
+ylabel('amplitude (V)')
+lg = legend('Fenetre de Hamming', ...
+	'Fenetre de Blackman', ...
+	'Location','Best');
+set(lg,'Interpreter','Latex');
+
+%4.
+
+S_x_welch = pwelch(x,'','','','twosided')';
+
+f_welch = linspace(0,Fe,length(S_x_welch));
+
+%Figure 9
+figure(9);
+plot(f_welch,S_x_welch);
+title('Estimation par périodogramme de Welch')
+xlabel('fréquence (Hz)')
+ylabel('amplitude (V)')
+
+%5.
+
+%Figure 10
+figure(10);
+plot(f,S_x,f,S_x_hamming,f,S_x_blackman,f_welch,S_x_welch);
+title('Estimation par périodogramme')
+xlabel('fréquence (Hz)')
+ylabel('amplitude (V)')
+lg = legend('Classique', ...
+	'Hamming', ...
+	'Blackman', ...
+	'Welch', ...
+	'Location','Best');
+set(lg,'Interpreter','Latex');
 
 
 
--- TP2.m
+++ TP2.m
@@ -1,184 +1,252 @@
-%%%%8.3%%%%
+%%%% 8.3 Implantation du modulateur %%%%
 
-Nb = 30;
-Fe = 1000;
-f0 = 200;
-Ns = 20;
-Te = 1/Fe;
-Ts = Ns*Te;
+Fe = 1000;  % Fréquence d'échantillonage
+f0 = 200;   % Fréquence du cosinus porteur
+A  = 1;     % Amplitude du cosinus porteur
+Nb = 30;    % Nombre d'éléments binaires à générer
+Ns = 20;    % Nombre d'échantillons par niveau
+Te = 1/Fe;  % Période d'échantillonage
+Ts = Ns*Te; % Durée d'un niveau
 
-%1.
-%(a)
+% 1.
+%   (a)
 
+%       Génération de l'information binaire à transmettre
 bits = randi([0,1],1,Nb);
 
-%(b)
+%   (b)
 
+%       Création du message à transmettre contenant l'information binaire
 m = kron(2*bits-1, ones(1,Ns));
 
-%(c)
+%   (c)
 
-x = linspace(0,Nb*Ts,Nb*Ns);
+%       Echelle temporelle du message
+t = linspace(0,Nb*Ts,Nb*Ns);
 
-%Figure 1
+% Figure 1 %
 figure(1);
-plot(x,m)
-title('Message Ã  transmettre')
+plot(t,m)
+title('Message à transmettre')
 xlabel('temps (s)')
 ylabel('amplitude')
 ylim([-1.5,1.5])
 
-%(d)
+%   (d)
 
-t = linspace(0,(Ns*Nb-1)/Fe,Ns*Nb);
-
+%       Calcul du module de la TFD du message
 M = fftshift(fft(m));
-
 Mabs = abs(M);
 
+%       Echelle fréquentielle de la TFD
 f = linspace(-Fe/2,Fe/2,Ns*Nb);
 
-%Figure 2
+% Figure 2 %
 figure(2);
 semilogy(f,Mabs);
 title('TFD du message')
-xlabel('frÃ©quence (Hz)')
+xlabel('fréquence (Hz)')
 ylabel('amplitude');
 
-%2.
+% 2.
 
-A = 1;
 
-%(a)
+%   (a)
 
+%       Echelle temporelle du cosinus porteur
+t = linspace(0,(Ns*Nb-1)/Fe,Ns*Nb);
+
+%       Modulation d'amplitude du message par le cosinus porteur
+%       permettant la transposition de fréquence en f0
 mod = A*cos(2*pi*f0*t);
 x = m.*mod;
 
-%(b)
+%   (b)
 
+%       Calcul du module de la TFD du message modulé
 X = fftshift(fft(x));
-
 Xabs = abs(X);
 
-%Figure 3
+% Figure 3 %
 figure(3);
 semilogy(f,Xabs);
-title('TFD du message modulÃ© une fois')
-xlabel('frÃ©quence (Hz)')
+title('TFD du message modulé une fois')
+xlabel('fréquence (Hz)')
 ylabel('amplitude')
-semilogy(f,Xabs);
 
-%%%%8.4%%%%
+%%%% 8.4 Implantation du retour à basse fréquence %%%%
 
-%1.
+% 1.
 
+%       Seconde modulation d'amplitude par le même cosinus que précédemment
+%       permettant le retour à basse fréquence
 y = x.*mod;
 
-%2.
+% 2.
 
+%       Calcul du module de la TFD du message modulé deux fois
 Y = fftshift(fft(y));
-
 Yabs = abs(Y);
 
-%Figure 4
+% Figure 4 %
 figure(4);
 semilogy(f,Yabs);
-title('TFD du message modulÃ© deux fois')
-xlabel('frÃ©quence (Hz)')
+title('TFD du message modulé deux fois')
+xlabel('fréquence (Hz)')
 ylabel('amplitude')
 
-%3.
+% 3.
 
-N = 100;
+N = 100; % Nombre entier arbitraire
 
-%(a)
+%   (a)
 
-k = linspace(-N*Te, N*Te, 2*N+1);
-h = sinc(f0*k);
+%       Echelle temporelle du morceau de réponse impulsionnelle idéale
+%       du filtre passe-bas
+t = linspace(-N*Te, N*Te, 2*N+1);
 
-%Figure 5
+%       Calcul d'un morceau de la réponse impulsionelle idéale du filtre
+%       passe-bas
+h = sinc(f0*t);
+
+% Figure 5 %
 figure(5);
-plot(k,h);
-title('RÃ©ponse impulsionnelle du filtre passe-bas idÃ©al')
+plot(t,h);
+title('Réponse impulsionnelle du filtre passe-bas idéal')
 xlabel('temps (s)')
 ylabel('amplitude')
 lg = legend('N = 100 (arbitraire)', ...
 	'Location','Best');
 set(lg,'Interpreter','Latex');
 
-%(b)
+%   (b)
 
+%       Calcul du module de la réponse en fréquence correspondant à la 
+%       réponse impulsionnelle calculée précédemment
 H = fftshift(fft(h));
-
 Habs = abs(H);
 
+%       Echelle fréquentielle de la réponse en fréquence
 f = linspace(-Fe,Fe,2*N+1);
 
-%Figure 6
+% Figure 6 %
 figure(6);
 semilogy(f,Habs);
-title('RÃ©ponse en frÃ©quence du filtre passe-bas idÃ©al')
-xlabel('frÃ©quence (Hz)')
+title('Réponse en fréquence du filtre passe-bas idéal')
+xlabel('fréquence (Hz)')
 ylabel('amplitude')
 lg = legend('N = 100 (arbitraire)', ...
 	'Location','Best');
 set(lg,'Interpreter','Latex');
 
-%(c)
+%   (c)
 
-N21 = 21;
-k21 = linspace(-N21*Te, N21*Te, 2*N21+1);
-h21 = sinc(f0*k21);
-H21 = fftshift(fft(h21));
-Habs21 = abs(H21);
-f21 = linspace(-Fe,Fe,2*N21+1);
+%       Calcul des réponses impulsionnelle et en fréquence (en module) du 
+%       filtre d'ordre 21 (avec échelles temporelle et fréquentielle
+%       correspondante)
+N1 = 21;
+t1 = linspace(-N1*Te, N1*Te, 2*N1+1);
+h1 = sinc(f0*t1);
+H1 = fftshift(fft(h1));
+Habs1 = abs(H1);
+f1 = linspace(-Fe,Fe,2*N1+1);
 
-N61 = 61;
-k61 = linspace(-N61*Te, N61*Te, 2*N61+1);
-h61 = sinc(f0*k61);
-H61 = fftshift(fft(h61));
-Habs61 = abs(H61);
-f61 = linspace(-Fe,Fe,2*N61+1);
+%       Calcul des réponses impulsionnelle et en fréquence (en module) du 
+%       filtre d'ordre 61 (avec échelles temporelle et fréquentielle
+%       correspondante)
+N2 = 61;
+t2 = linspace(-N2*Te, N2*Te, 2*N2+1);
+h2 = sinc(f0*t2);
+H2 = fftshift(fft(h2));
+Habs2 = abs(H2);
+f2 = linspace(-Fe,Fe,2*N2+1);
 
-%Figure 7
+% Figure 7 %
 figure(7);
-hold on;
-plot(k21,h21);
-plot(k61,h61);
-title('RÃ©ponse impulsionnelle du filtre passe-bas idÃ©al')
+plot(t2,h2,t1,h1);
+title('Réponse impulsionnelle du filtre passe-bas idéal')
 xlabel('temps (s)')
 ylabel('amplitude')
 lg = legend('N = 21', ...
 	'N = 61', ...
 	'Location','Best');
 set(lg,'Interpreter','Latex');
-hold off;
 
-%Figure 8
+% Figure 8 %
 figure(8);
-hold on;
-semilogy(f21,Habs21);
-semilogy(f61,Habs61);
-title('RÃ©ponse en frÃ©quence du filtre passe-bas idÃ©al')
-xlabel('frÃ©quence (Hz)')
+semilogy(f1,Habs1,f2,Habs2);
+title('Réponse en fréquence du filtre passe-bas idéal')
+xlabel('fréquence (Hz)')
 ylabel('amplitude')
 lg = legend('N = 21', ...
 	'N = 61', ...
 	'Location','Best');
 set(lg,'Interpreter','Latex');
-hold off;
 
-%(d)
+%   (d)
 
+%       Calcul des réponses impulsionnelle et fréquentielle du filtre
+%       passe-bas d'ordre 61 avec fenêtre de blackman
+w = blackman(2*N2+1)';
+hf = h2.*w;
+Hf = fftshift(fft(hf));
+Hfabs = abs(Hf);
 
+% Figure 9 %
+figure(9);
+plot(t2,h2,t2,hf);
+title('Réponse impulsionnelle du filtre passe-bas idéal fenêtré, N = 61')
+xlabel('temps (s)')
+ylabel('amplitude')
+lg = legend('Fenetre rectangulaire', ...
+	'Fenetre de Blackman', ...
+	'Location','Best');
+set(lg,'Interpreter','Latex');
 
+% Figure 10 %
+figure(10);
+semilogy(f2,Habs2,f2,Hfabs);
+title('Réponse en fréquence du filtre passe-bas idéal fenêtré')
+xlabel('fréquence (Hz)')
+ylabel('amplitude')
+lg = legend('Fenetre rectangulaire', ...
+	'Fenetre de Blackman', ...
+	'Location','Best');
+set(lg,'Interpreter','Latex');
 
+%   (e)
 
+%       Echelle fréquentielle de la TFD du message modulé deux fois
+f = linspace(-Fe/2,Fe/2,Ns*Nb);
 
+% Figure 11 %
+figure(11);
+semilogy(f,Yabs,f2,Hfabs);
+title('Spectre du signal à filtrer et filtre')
+xlabel('fréquence (Hz)')
+ylabel('amplitude')
+lg = legend('Spectre du signal a filtrer', ...
+	'Reponse en frequence du filtre', ...
+	'Location','Best');
+set(lg,'Interpreter','Latex');
 
+%   (f)
 
+%        Application du filtre sur le message modulé deux fois
+mf = conv(y,hf,'same');
 
+%       Echelle temporelle du message
+t = linspace(0,Nb*Ts,Nb*Ns);
 
-
+% Figure 12 %
+figure(12);
+plot(t,m,t,mf);
+title('Message transmis et signal en sortie de filtre')
+xlabel('temps (s)')
+ylabel('amplitude')
+lg = legend('Message transmis', ...
+	'Signal en sortie de filtre', ...
+	'Location','Best');
+set(lg,'Interpreter','Latex');
 
 
